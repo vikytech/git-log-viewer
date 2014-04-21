81668ad1c89dc58b4f6f596afd636af5bc83bbbf
Prasanth <prasantd@thoughtworks.com>
2014-04-21 17:27:28 +0530

    Added link to each commit.

--- app.js
+++ app.js
@@ -24,11 +24,10 @@ app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/home', home);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -39,7 +38,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -50,7 +49,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var git = require('../git.js');
-
 router.get('/', function (req, res) {
     git.getCommits(function (result, commits) {
         console.log(commits[0]);
         res.render('home', { commits: commits});
     });
+});
+router.get('/commit', function (req, res) {
+    res.render('index', {hash: req.param("hash")});
 });
 
 module.exports = router;
